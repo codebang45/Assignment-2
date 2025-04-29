2d3470d9cf639d70a7734685865c3206b1c000c1
codebang45 <indoliajatin4@gmail.com>
2025-04-29 20:27:18 +0530

    add the stringtoolkit.h

---
+++ stringtoolkit.h
@@ -0,0 +1,11 @@
+#ifndef STRINGTOOLKIT_H
+#define STRINGTOOLKIT_H
+
+void to_uppercase(char str[]);
+void to_lowercase(char str[]);
+int count_vowels(const char str[]);
+void reverse_string(char str[]);
+void sort_strings(char arr[][100], int n);
+void reverse_words(char str[]);
+
+#endif
